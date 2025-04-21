93bdcb5ce99a09b5e5b9778a87ce94d60fa0116d
yashgoel <yash.goel8370@gmail.com>
2025-04-21 19:53:55 +0530

    commit

--- app/Header.js
+++ app/Header.js
@@ -3,33 +3,7 @@ import "./Header.css";
 function Header() {
   return (
     <>
-      <div className="header-container">
-        <div className="header-buttons">
-          <div className="header-red"></div>
-          <div className="header-yellow"></div>
-          <div className="header-green"></div>
-        </div>
-        <div className="header-content">
-          <code>
-            &lt;html&gt;<br></br>
-            &nbsp;&lt;head&gt;<br></br>
-            &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;<br></br>
-            &nbsp;&nbsp;&lt;style&gt;<br></br>
-            &nbsp;&nbsp;&nbsp;.content &#123;<br></br>
-            &nbsp;&nbsp;&nbsp;&nbsp; font-size: 50px; text-align: center;
-            font-family: &apos;Courier New&apos;, Courier, monospace;<br></br>
-            &nbsp;&nbsp;&nbsp;&#125;<br></br>
-            &nbsp;&nbsp;&lt;/style&gt;<br></br>
-            &nbsp;&lt;/head&gt;<br></br>
-            &nbsp;&lt;body&gt;<br></br>
-            &nbsp;&nbsp;&lt;div class=&ldquo;content&ldquo;&gt;<br></br>
-            &nbsp;&nbsp;&nbsp;Hello, I am Yash Goel!<br></br>
-            &nbsp;&nbsp;&lt;/div&gt;<br></br>
-            &nbsp;&lt;/body&gt;<br></br>
-            &lt;/html&gt;
-          </code>
-        </div>
-      </div>
+     
     </>
   );
 }
--- app/Main.js
+++ app/Main.js
@@ -1,14 +1,96 @@
-import "./Main.css"
+"use client";
+
+import "./Main.css";
+import { useState, useEffect } from "react";
+
 function Main() {
-    
-    return (
-        <>
-            <div className="main-container">
-                <div className="main-content">
-                    &gt;&gt;&gt; Hello, I am Yash Goel!
-                </div>
+  const [isHovered, setIsHovered] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  // Update isDesktop based on screen width
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    handleResize(); // Run initially
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <>
+      <div className={`Hero-container ${isHovered ? "hovered" : ""}`}>
+        <div className="header-container">
+          <div className="header-buttons">
+            <div className="header-red"></div>
+            <div className="header-yellow"></div>
+            <div className="header-green"></div>
+          </div>
+          <div className={`Header-content ${isHovered ? "hovered" : ""}`}>
+            {isHovered ? (
+              <code>
+                &lt;html&gt;<br />
+                &nbsp;&lt;head&gt;<br />
+                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;<br />
+                &nbsp;&nbsp;&lt;style&gt;<br />
+                &nbsp;&nbsp;&nbsp;.content &#123;<br />
+                &nbsp;&nbsp;&nbsp;&nbsp; font-size: 50px; text-align: center;
+                font-family: &apos;Courier New&apos;, Courier, monospace;<br />
+                &nbsp;&nbsp;&nbsp;&#125;<br />
+                &nbsp;&nbsp;&nbsp;.name:hover &#123;<br />
+                &nbsp;&nbsp;&nbsp;&nbsp; background-color: white;
+                color: black;<br />
+                &nbsp;&nbsp;&nbsp;&#125;<br />
+                &nbsp;&nbsp;&lt;/style&gt;<br />
+                &nbsp;&lt;/head&gt;<br />
+                &nbsp;&lt;body&gt;<br />
+                &nbsp;&nbsp;&lt;div class="content"&gt;<br />
+                &nbsp;&nbsp;&nbsp;Hello, I am &lt;span class="name"&gt;Yash
+                Goel!&lt;/span&gt;<br />
+                &nbsp;&nbsp;&lt;/div&gt;<br />
+                &nbsp;&lt;/body&gt;<br />
+                &lt;/html&gt;
+              </code>
+            ) : (
+              <code>
+                &lt;html&gt;<br />
+                &nbsp;&lt;head&gt;<br />
+                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;<br />
+                &nbsp;&nbsp;&lt;style&gt;<br />
+                &nbsp;&nbsp;&nbsp;.content &#123;<br />
+                &nbsp;&nbsp;&nbsp;&nbsp; font-size: 50px; text-align: center;
+                font-family: &apos;Courier New&apos;, Courier, monospace;<br />
+                &nbsp;&nbsp;&nbsp;&#125;<br />
+                &nbsp;&nbsp;&lt;/style&gt;<br />
+                &nbsp;&lt;/head&gt;<br />
+                &nbsp;&lt;body&gt;<br />
+                &nbsp;&nbsp;&lt;div class="content"&gt;<br />
+                &nbsp;&nbsp;&nbsp;Hello, I am Yash Goel!<br />
+                &nbsp;&nbsp;&lt;/div&gt;<br />
+                &nbsp;&lt;/body&gt;<br />
+                &lt;/html&gt;
+              </code>
+            )}
+          </div>
         </div>
-        </>
-    )
+        <div className="main-container">
+          <div className="main-content">
+            &gt;&gt;&gt; Hello, I am{" "}
+            <span
+              className="name"
+              onMouseEnter={() => isDesktop && setIsHovered(true)}
+              onMouseLeave={() => isDesktop && setIsHovered(false)}
+            >
+              Yash Goel
+            </span>
+            !
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Main;
