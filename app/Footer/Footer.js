f1159afb6a5257b297320a4665e4446a5ab739fa
yashgoel <yash.goel8370@gmail.com>
2025-04-29 23:22:23 +0530

    commit

--- app/Footer/Footer.js
+++ app/Footer/Footer.js
@@ -2,13 +2,14 @@ import "./Footer.css"
 import Image from "next/image"
 import linkedin from "../assets/linkedin.png"
 import github from "../assets/github.png"
-
+import logo from "../assets/logo.png"
 
 function Footer() {
     return (
         <>
             <div className="Footer-container">
-                <div className="Footer-content">© {new Date().getFullYear()} Yash Goel - yashgoel75</div>
+                <div className="Footer-content"><Image src={logo} width={70} height={70}></Image>
+                    © {new Date().getFullYear()} Yash Goel - yashgoel75</div>
                 <div className="Footer-social">
                     <a href="https://www.linkedin.com/in/yashgoel75/" target="_blank"><Image src={linkedin} width={30} height={30} alt="LinkedIn"></Image></a>
                     &nbsp;&nbsp;&nbsp;&nbsp;
