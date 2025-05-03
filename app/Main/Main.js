58beda75b5e9765c64b56591cb2d8d9912c7665c
yashgoel <yash.goel8370@gmail.com>
2025-05-03 14:54:43 +0530

    commit

--- app/Main/Main.js
+++ app/Main/Main.js
@@ -30,49 +30,82 @@ function Main() {
           <div className={"Header-content"}>
             {isDesktop ? (
               <code>
-                &lt;html&gt;<br />
-                &nbsp;&lt;head&gt;<br />
-                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;<br />
-                &nbsp;&nbsp;&lt;style&gt;<br />
-                &nbsp;&nbsp;&nbsp;.content &#123;<br />
+                &lt;html&gt;
+                <br />
+                &nbsp;&lt;head&gt;
+                <br />
+                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;
+                <br />
+                &nbsp;&nbsp;&lt;style&gt;
+                <br />
+                &nbsp;&nbsp;&nbsp;.content &#123;
+                <br />
                 &nbsp;&nbsp;&nbsp;&nbsp; font-size: 50px; text-align: center;
-                font-family: &apos;Courier New&apos;, Courier, monospace;<br />
-                &nbsp;&nbsp;&nbsp;&#125;<br />
-                &nbsp;&nbsp;&nbsp;.name &#123;<br />
+                font-family: &apos;Courier New&apos;, Courier, monospace;
+                <br />
+                &nbsp;&nbsp;&nbsp;&#125;
+                <br />
+                &nbsp;&nbsp;&nbsp;.name &#123;
+                <br />
                 &nbsp;&nbsp;&nbsp;&nbsp; transition: 0.3s ease;
                 <br />
-                &nbsp;&nbsp;&nbsp;&#125;<br />
-                &nbsp;&nbsp;&nbsp;.name:hover &#123;<br />
-                &nbsp;&nbsp;&nbsp;&nbsp; background-color: white;
-                color: black; cursor: pointer;<br />
-                &nbsp;&nbsp;&nbsp;&#125;<br />
-                &nbsp;&nbsp;&lt;/style&gt;<br />
-                &nbsp;&lt;/head&gt;<br />
-                &nbsp;&lt;body&gt;<br />
-                &nbsp;&nbsp;&lt;div class=&quot;content&quot;&gt;<br />
-                &nbsp;&nbsp;&nbsp;Hello, I am &lt;span class=&quot;name&quot;&gt;Yash
-                Goel&lt;/span&gt;!<br />
-                &nbsp;&nbsp;&lt;/div&gt;<br />
-                &nbsp;&lt;/body&gt;<br />
+                &nbsp;&nbsp;&nbsp;&#125;
+                <br />
+                &nbsp;&nbsp;&nbsp;.name:hover &#123;
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp; background-color: white; color: black;
+                cursor: pointer;
+                <br />
+                &nbsp;&nbsp;&nbsp;&#125;
+                <br />
+                &nbsp;&nbsp;&lt;/style&gt;
+                <br />
+                &nbsp;&lt;/head&gt;
+                <br />
+                &nbsp;&lt;body&gt;
+                <br />
+                &nbsp;&nbsp;&lt;div class=&quot;content&quot;&gt;
+                <br />
+                &nbsp;&nbsp;&nbsp;Hello, I am &lt;span
+                class=&quot;name&quot;&gt;Yash Goel&lt;/span&gt;!
+                <br />
+                &nbsp;&nbsp;&lt;/div&gt;
+                <br />
+                &nbsp;&lt;/body&gt;
+                <br />
                 &lt;/html&gt;
               </code>
             ) : (
               <code>
-                &lt;html&gt;<br />
-                &nbsp;&lt;head&gt;<br />
-                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;<br />
-                &nbsp;&nbsp;&lt;style&gt;<br />
-                &nbsp;&nbsp;&nbsp;.content &#123;<br />
-                &nbsp;&nbsp;&nbsp;&nbsp; font-size: 50px; text-align: center;
-                font-family: &apos;Courier New&apos;, Courier, monospace;<br />
-                &nbsp;&nbsp;&nbsp;&#125;<br />
-                &nbsp;&nbsp;&lt;/style&gt;<br />
-                &nbsp;&lt;/head&gt;<br />
-                &nbsp;&lt;body&gt;<br />
-                &nbsp;&nbsp;&lt;div class=&quot;content&quot;&gt;<br />
-                &nbsp;&nbsp;&nbsp;Hello, I am Yash Goel!<br />
-                &nbsp;&nbsp;&lt;/div&gt;<br />
-                &nbsp;&lt;/body&gt;<br />
+                &lt;html&gt;
+                <br />
+                &nbsp;&lt;head&gt;
+                <br />
+                &nbsp;&nbsp;&lt;title&gt; Yash Goel &lt;/title&gt;
+                <br />
+                &nbsp;&nbsp;&lt;style&gt;
+                <br />
+                &nbsp;&nbsp;&nbsp;.content &#123;
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp; font-size: 32px; text-align: center;
+                font-family: &apos;Courier New&apos;, Courier, monospace;
+                <br />
+                &nbsp;&nbsp;&nbsp;&#125;
+                <br />
+                &nbsp;&nbsp;&lt;/style&gt;
+                <br />
+                &nbsp;&lt;/head&gt;
+                <br />
+                &nbsp;&lt;body&gt;
+                <br />
+                &nbsp;&nbsp;&lt;div class=&quot;content&quot;&gt;
+                <br />
+                &nbsp;&nbsp;&nbsp;Hello, I am Yash Goel!
+                <br />
+                &nbsp;&nbsp;&lt;/div&gt;
+                <br />
+                &nbsp;&lt;/body&gt;
+                <br />
                 &lt;/html&gt;
               </code>
             )}
@@ -81,16 +114,10 @@ function Main() {
         <div className="main-container">
           <div className="main-content">
             &gt;&gt;&gt; Hello, I am&nbsp;
-            <span
-              className="name"
-            >
-              Yash Goel
-            </span>
-            !
+            <span className="name">Yash Goel</span>!
           </div>
         </div>
         {/* <GitHubCalendar username="yashgoel75" /> */}
-
       </div>
     </>
   );
