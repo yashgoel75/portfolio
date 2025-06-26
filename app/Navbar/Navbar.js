05712e09bc30ec846cd3b586461fd0f862804fcc
yashgoel <yash.goel8370@gmail.com>
2025-06-26 22:56:10 +0530

    commit

--- app/Navbar/Navbar.js
+++ app/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 "use client";
 import "./Navbar.css";
 import { useState } from "react";
-
+import downloadbutton from "../assets/downloadbutton.png";
+import Image from "next/image";
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -51,17 +52,7 @@ function Navbar() {
                 rel="noopener noreferrer"
               >
                 <div className="resume-text">
-                  RESUME{" "}
-                  <svg
-                    id="download-image"
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 -960 960 960"
-                    width="24px"
-                    fill="#101010"
-                  >
-                    <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
-                  </svg>
+                  RESUME
                 </div>
               </a>
             </li>
